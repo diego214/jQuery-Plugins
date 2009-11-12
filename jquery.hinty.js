1742066163c95bbc435ab9ef3a7f1a0d99587295
diego214 <mastermindxs@gmail.com>
2009-11-12 08:45:51 -0700

    changed event for making the hint text disappear from a focus event to a kewdown event.
    when the hint text is active, clicking or focusing on the field sets the caret position to 0, making it appear the hint text is unselectable.

--- jquery.hinty.js
+++ jquery.hinty.js
@@ -3,8 +3,8 @@
  * Hinty by Diego Salazar, oct. 2009
  * diego at greyrobot dot com
  */
+// applies a text hint to the field by using the title attribute
 jQuery.fn.hinty = function() {
-	// applies a text hint to the text input field by using the input's title attribute
 	return this.each(function(){
 		var $this = jQuery(this);
 		var title = this.title;
@@ -12,13 +12,32 @@ jQuery.fn.hinty = function() {
 		$this.addClass('hint_text');
 		$this.val(title);
 		
-		$this.focus(function(){
+		// set the caret position to 0, making it appear that the hint text is non selectable text
+		$this.focus(setCaretPost).click(setCaretPost);
+		function setCaretPost(){
+			if (this.value == title){
+				if (this.setSelectionRange) {
+					this.focus();
+					this.setSelectionRange(0, 0);
+				} else if (this.createTextRange) {
+					var range = this.createTextRange();
+					range.collapse(true);
+					range.moveEnd('character', 0);
+					range.moveStart('character', 0);
+					range.select();
+				}
+			}
+		}
+		
+		// get rid of hint text when a key is pressed on the input
+		$this.keydown(function(){
 			if ($this.val() == title) {
 				$this.removeClass('hint_text');
 				$this.val('');
 			}
 		});
 		
+		// put the hint text back if the input is blank
 		$this.blur(function(){
 			if ($this.val() == '') {
 				$this.addClass('hint_text');
